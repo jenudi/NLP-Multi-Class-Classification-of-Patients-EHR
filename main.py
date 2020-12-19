130fbc575d47f69efe2af67fca0a45847262caf9
jenudi <jony101@gmail.com>
2020-12-20 00:46:30 +0200

    first python and data files

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
+#%% Imports
+
 import pandas as pd
 import re
 from nltk.corpus import stopwords
-from nltk.stem import SnowballStemmer
 from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from collections import OrderedDict
+
+from nltk.stem import SnowballStemmer
 from collections import Counter
 import copy
 from nltk.util import ngrams
 import nltk
 import numpy as np
 
-try:
-    _ = stopwords.words("english")
-except LookupError:
-    import nltk
-    nltk.download('stopwords')
-
-stemmer = PorterStemmer()
-#stemmer = SnowballStemmer('english')
-stopword_set = set(stopwords.words("english"))
-
+#%% Loading Data
 
 encounter_data = pd.read_csv("encounter.csv").rename(str.lower, axis='columns')
 encounter_dx = pd.read_csv("encounter_dx.csv").rename(str.lower, axis='columns')
@@ -36,8 +31,20 @@ soap = data['soap_note'].dropna().reset_index(drop=True)
 soap_temp = [re.split('o:''|''o :', i) for i in soap] # split by "o:" or "o :"
 temp_sentences = [i[0].strip().strip('s:').lower() for i in soap_temp]
 
-document = '\n '.join(temp_sentences)
 
+#%% Preprocessing
+
+try:
+    _ = stopwords.words("english")
+except LookupError:
+    import nltk
+    nltk.download('stopwords')
+
+stopword_set = set(stopwords.words("english"))
+stemmer = PorterStemmer()
+#stemmer = SnowballStemmer('english')
+
+document = '\n '.join(temp_sentences)
 document = document.replace(r"'s", "  is")
 document = document.replace(r"'ve", " have")
 document = document.replace(r"can't", "cannot")
@@ -95,14 +102,11 @@ document = document.replace(r" pt ", " patient ")
 document = document.replace(r" yo ", " years old ")
 document = document.replace(r" yr ", " years old ")
 document = document.replace(r" x ", " times ")
-
 document = document.split('\n ')
 
 if stemmer:
     for i, sentence in enumerate(document):
         document[i] = ' '.join([stemmer.stem(w) for w in sentence.split() if w not in stopword_set])
-
-#%%
 
 sentences = [re.split(r'[-\s.,;!?]+', i)[:-1] for i in document]
 
@@ -111,16 +115,35 @@ sentences = [re.split(r'[-\s.,;!?]+', i)[:-1] for i in document]
 #    corpus['sent' + str(i)] = Counter(sentence)
 #df = pd.DataFrame.from_records(corpus).fillna(0).astype(int).T
 
+#%% TF-IDF
 
-#%%
 doc_tokens = []
 for sent in sentences:
     doc_tokens += [sorted(sent)]
 lexicon = sorted(set(sum(doc_tokens, [])))
-
 zero_vector = OrderedDict((token, 0) for token in lexicon)
-document_tfidf_vectors = []
 
+corpus = {}
+for i, sentence in enumerate(sentences):
+    corpus['sent' + str(i)] = ' '.join(sentence)
+
+tfidf = TfidfVectorizer(min_df=0.0,smooth_idf=True)
+tfs = tfidf.fit_transform(corpus.values())
+
+#%%
+
+docs = ["The faster Harry got to the store, the faster and faster Harry would get home."]
+docs.append("Harry is hairy and faster than Jill.")
+docs.append("Jill is not as hairy as Harry.")
+
+l = []
+for i in sentences:
+    l.append(' '.join(i))
+tfs2 = tfidf.fit_transform(l)
+
+#%% TF-IDF Manual
+"""
+document_tfidf_vectors = []
 for sent in sentences:
     vec = copy.copy(zero_vector)
     tokens = sent
@@ -143,17 +166,7 @@ corpus = {}
 for i, n in enumerate(document_tfidf_vectors):
     corpus['sent' + str(i)] = n
 df = pd.DataFrame.from_records(corpus).T
-
-#%%
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-corpus = {}
-for i, sentence in enumerate(sentences):
-    corpus['sent' + str(i)] = ' '.join(sentence)
-
-tfidf = TfidfVectorizer(min_df=0.0,smooth_idf=True)
-tfs = tfidf.fit_transform(corpus.values())
-
+"""
 #%%
 #sentences = [re.split(r'[-\s.,;!?]+', i) for i in temp_sentences]
 #for i, token in enumerate(sentences):
@@ -168,6 +181,6 @@ tfs = tfidf.fit_transform(corpus.values())
  #   corpus['sent' + str(i)] = dict((token, 1) for token in sentence)
 #df = pd.DataFrame.from_records(corpus).fillna(0).astype(int).T
 
-# Two
+# Two-grams
 #tokens_two = list(ngrams(i, 2) for i in sentences)
 
