11164597ed6bdea52bbce9554c59b7fc662a8e9f
EladGashri <eladgashri@gmail.com>
2021-03-08 13:07:44 +0200

    update

--- main.py
+++ main.py
@@ -47,10 +47,9 @@ for model in args.models:
         document.train.make_word2vec_for_rnn(args, None)
     rnn = RNN(args.word2vec_vec_size_for_rnn, args.hidden_layer, len(document.labels_dict))
     training = TrainValidate(args,document,rnn)
-    eval_rnn[f'y_pred_{document.train.word2vec_model_name}_{args.lr}_{args.hidden_layer}'] = training.main(continue_training=False,
-                                                                                                           decay_learning=False)
+    eval_rnn[f'y_pred_{document.train.word2vec_model_name}_{args.lr}_{args.hidden_layer}'] = training.main(continue_training=False,decay_learning=False)
 
-#random_forest_test_score=f1_score(eval_rnn.iloc[:,0], eval_rnn.iloc[:,1], average='micro')
+#rnn_test_score=f1_score(eval_rnn.iloc[:,0], eval_rnn.iloc[:,1], average='micro')
 
 
 
@@ -58,7 +57,6 @@ for model in args.models:
 random.shuffle(document.train.sentences)
 random.shuffle(document.test.sentences)
 random.shuffle(document.validation.sentences)
-
 
 tfidf_model = TfidfVectorizer(min_df=args.min,smooth_idf=True,norm='l1')
 tfidf_trained = tfidf_model.fit(document.train.get_sentences())
@@ -74,6 +72,7 @@ train_labels = [sentence.label for sentence in document.train.sentences]
 validation_labels = [sentence.label for sentence in document.validation.sentences]
 n_estimators_list=[100,200,300,400,500,1000,3000,5000]
 random_forest_validation_scores=list()
+
 for n_estimators in n_estimators_list:
     random_forest_model = RandomForestClassifier(n_estimators=n_estimators, criterion='gini', max_depth=None, bootstrap=True, class_weight="balanced_subsample", random_state=0)
     _ = random_forest_model.fit(document.train.tfidf,train_labels)
@@ -87,10 +86,10 @@ print("the best n_estimator is "+ str(best_n_estimator) + " with score of "+ str
 chosen_random_forest_model=RandomForestClassifier(n_estimators=n_estimators, criterion='gini', max_depth=None, bootstrap=True, class_weight="balanced_subsample", random_state=0)
 _ = chosen_random_forest_model.fit(document.train.tfidf, train_labels)
 
-pickle.dump(chosen_random_forest_model, open("random_forest_model.pkl", "wb"))
-
 document.test.make_tfidf(tfidf_trained)
 test_labels = [sentence.label for sentence in document.test.sentences]
 predicted_test_labels = chosen_random_forest_model.predict(document.test.tfidf)
 random_forest_test_score=f1_score(test_labels,predicted_test_labels,average='micro')
 print("test score of random forest is " + str(random_forest_test_score))
+
+pickle.dump(chosen_random_forest_model, open("random_forest_model.pkl", "wb"))
